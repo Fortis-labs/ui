2e93375cd45aa85461189ac891be0fb37fcc1323
mubariz <mubarizharoonmulpk@gmail.com>
2025-12-27 15:45:39 +0500

    adjust tx import bug

--- src/lib/transaction/simulateEncodedTransaction.ts
+++ src/lib/transaction/simulateEncodedTransaction.ts
@@ -15,10 +15,10 @@ export const simulateEncodedTransaction = async (
   }
   try {
     const { message, version } = decodeAndDeserialize(tx);
-
+    console.log("message version", version);
     const transaction = new VersionedTransaction(message.compileToV0Message());
 
-    const keys = await getAccountsForSimulation(connection, transaction, version === 0);
+    const keys = await getAccountsForSimulation(connection, transaction, version === 'legacy');
 
     toast.loading('Simulating...', {
       id: 'simulation',
@@ -42,3 +42,5 @@ export const simulateEncodedTransaction = async (
     throw new Error(error);
   }
 };
+//4XXJuXUSqBGQprPkKNGgWqDbxW7eqQiN8Ubvwrc384HYiQ8mRfjgK8MGLEurzzCm7azmioKLhQgaxcx3C5neexMEcW9ZZ6TcrJMw2mLX8gNbusZwvYYgMvTFz5yvcNYUqdy2pVCDisR1JpE1d28HLavkiNQHXkcGN46HoTSenMmBamTRaE3y7Wa7AnUiwoBe3qw3Pbn72cdNxuvRpDfAbqY8kG
+//6GhS2ZJaE317xrcfP9CeXR76R6fUa3RkCZyYZeLiKHtcc91ygBKr9ShtEYY9QZvJfCjV1AErrDMbDRnGX2sQwHXdEhsRfJpPNoANPaAqB4t4h8CpWeBiX1K7geuTTJGrbgaGPKTRguphrcudw33AqMY7aPx2tyVmHpM2sddS2BBRAWRM3NvQdY9C1Qo5K59mSpwX3r8TNQus3ZQT
